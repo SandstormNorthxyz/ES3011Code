d029350b96beb8d1961209d3725641cd68c8c233
SandstormNorth <collingkeegan@gmail.com>
2024-12-11 16:31:29 -0500

    pid constants

--- src/main.cpp
+++ src/main.cpp
@@ -30,9 +30,9 @@ uint32_t rightPos = 0;
 float leftSetpoint = 0;
 float rightSetpoint = 0;
 
-float P = 0;
+float P = 0.5;
 float I = 0;
-float D = 0;
+float D = 0.006;
 // SmartMotor motors[] = {0x05,0x06,0x07}; // INIT MOTOR W/ DEFAULT ADDRESS
 
 vec2<float> pos;
@@ -88,9 +88,9 @@ void setup() {
     Telemetry::initPacket(Telemetry::leftSetpoint, &leftSetpoint);
     Telemetry::initPacket(Telemetry::rightSetpoint, &rightSetpoint);
 
-  Telemetry::initPacket(Telemetry::P, &P);
-  Telemetry::initPacket(Telemetry::I, &I);
-  Telemetry::initPacket(Telemetry::D, &D);
+    Telemetry::initPacket(Telemetry::P, &P);
+    Telemetry::initPacket(Telemetry::I, &I);
+    Telemetry::initPacket(Telemetry::D, &D);
   #endif
 }
 
@@ -101,6 +101,8 @@ void loop() {
   // delay(20);
 
   leftMotor.tune_vel_pid(P, I, D);
+  rightMotor.tune_vel_pid(P, I, D);
+
   leftStatus = leftMotor.set_rpm(leftSetpoint);
   rightStatus = rightMotor.set_rpm(rightSetpoint);
   
